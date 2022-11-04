1c35afbe34c404240417ec416bffa74c6eb26d74
Grigoriy Belikov <belikovgrisha9@gmail.com>
2022-11-04 20:44:21 +0300

    first commit

---
+++ static/scripts.js
@@ -0,0 +1,17 @@
+let buttons = [
+    ("Lorem, ipsum dolor", 0),
+    ("sit amet", 1),
+    ("consectetur adipisicing elit. Vero asperiores doloribus", 2),
+    ("corporis sit deserunt libero", 0),
+    ("a deleniti", 1),
+    ("enim quod neque", 2),
+    ("commodi odio modi dolore", 0),
+    ("maiores debitis quasi", 1),
+    ("nemo ut eveniet", 2)
+]
+
+let button_styles=[
+    "btn-outline-success",
+    "btn-outline-primary",
+    "btn-outline-danger"
+]
