70ff4523d03ae0f2c5b6884015a9ede893ecf45e
Grigoriy Belikov <belikovgrisha9@gmail.com>
2022-11-05 14:46:00 +0300

    Подчистил js

--- static/scripts.js
+++ static/scripts.js
@@ -21,16 +21,13 @@ let button_styles_active=[
     "btn-danger"
 ]
 
-
 $(document).ready(function(){
 
     let used_ids = [];
     let generated_input = "";
-    let user_input = "";
 
     function update_result(){
         generated_input = "";
-        console.log(user_input)
         for (let i = 0; i < buttons.length; ++ i) {
             if (used_ids[i]){
                 generated_input += buttons[i][0] + " ";
@@ -93,29 +90,4 @@ $(document).ready(function(){
             res_field.removeClass("outline");
         }
     })
-
-    // $(document).click(function(e){
-    //     let res_field = $(".form-control");
-    //     input_focus = document.elementFromPoint(e.clientX, e.clientY) == res_field[0]
-    //     if (input_focus){
-    //         res_field.addClass("in_focus");
-    //     } else {
-    //         res_field.removeClass("in_focus");
-    //     }
-    // })
-    // $(document).keydown(function(e){
-    //     if (input_focus){
-    //         if (e.key.length == 1){
-    //             user_input += e.key;
-    //         } else if (e.key == "Enter"){
-    //             user_input += "\n";
-    //         } else if (e.key == "Backspace"){
-    //             user_input = user_input.slice(0, user_input.length - 1)
-    //         }
-    //         console.log(e.key)
-    //         update_result();
-    //     }
-    // })
-
-
 });
