75243c43a60fdf628bc8990c30972bf5707f4f39
Grigoriy Belikov <belikovgrisha9@gmail.com>
2022-11-08 21:52:00 +0300

    Заменил плейсхолдеры на нормальный текст

--- static/scripts.js
+++ static/scripts.js
@@ -1,13 +1,13 @@
 let buttons = [
-    ["Lorem, ipsum dolor", 0],
-    ["sit amet", 1],
-    ["consectetur adipisicing elit. Vero asperiores doloribus", 2],
-    ["corporis sit deserunt libero", 0],
-    ["a deleniti", 1],
-    ["enim quod neque", 2],
-    ["commodi odio modi dolore", 0],
-    ["maiores debitis quasi", 1],
-    ["nemo ut eveniet", 2]
+    ["Быстрый шиномонтаж.", 2],
+    ["Оперативное обслуживание.", 2],
+    ["Бесплатный кофе в зоне ожидания.", 2],
+    ["Большой центр, много постов.", 2],
+    ["Приветливый персонал.", 2],
+    ["Много услуг автосервиса.", 2],
+    ["Класс!", 0],
+    ["Молодцы!", 0],
+    ["Спасибо!", 0]
 ]
 
 let button_styles_inactive=[
